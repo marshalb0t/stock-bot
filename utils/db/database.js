a634e13e2b22ccf39e9e76d83a0779410c2b9e12
MrBlackJack735 <74959944+black-jack735@users.noreply.github.com>
2024-11-22 00:04:08 +0300

    Update database.js

--- utils/db/database.js
+++ utils/db/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoUrI);
+    await mongoose.connect("mongodb+srv://clever_mongodb:clever1999@cluster0.6e4mf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB connected!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
